b3133212fd003e56c75d80fd9311e8f64edc67b4
Jakub Orava <Jakuborava@gmail.com>
2026-01-19 15:58:20 +0100

    Offer endpoints

--- src/Endpoints/AffiliateEndpoints.php
+++ src/Endpoints/AffiliateEndpoints.php
@@ -6,6 +6,7 @@ namespace JakubOrava\ScaleoIoClient\Endpoints;
 
 use JakubOrava\ScaleoIoClient\BaseScaleoClient;
 use JakubOrava\ScaleoIoClient\Endpoints\Affiliate\Dashboard;
+use JakubOrava\ScaleoIoClient\Endpoints\Affiliate\Offers;
 
 class AffiliateEndpoints
 {
@@ -18,8 +19,12 @@ class AffiliateEndpoints
         return new Dashboard($this->client);
     }
 
+    public function offers(): Offers
+    {
+        return new Offers($this->client);
+    }
+
     // TODO: Add endpoint methods as they are implemented
-    // public function offers(): Offers
     // public function offerRequests(): OfferRequests
     // public function reports(): ReportsEndpoints
     // public function billing(): BillingEndpoints
